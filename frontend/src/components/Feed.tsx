1a222738b193ed5d010baea2ec6c177da24f335c
Ayantik Sarkar <99005057+ayantik2006@users.noreply.github.com>
2025-09-27 14:16:16 +0530

    update Feed component

--- frontend/src/components/Feed.tsx
+++ frontend/src/components/Feed.tsx
@@ -1,4 +1,30 @@
+import { useEffect } from "react";
+import { useLocation, useNavigate } from "react-router-dom";
+
 function Feed() {
+  const navigate = useNavigate();
+  const location = useLocation();
+  const backendURL = "https://project-base-backend.onrender.com";
+
+  useEffect(() => {
+      fetch(backendURL + "/auth/user", {
+        method: "POST",
+        credentials: "include",
+        headers: { "Content-Type": "application/json" },
+      })
+        .then((res) => res.json())
+        .then((res) => {
+          if (res.msg === "logged out") {
+            navigate("/signin");
+          } else if (res.msg !== "logged out") {
+            navigate("/feed");
+          }
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    }, [navigate, location.pathname]);
+  
   return (
     <div>Feed</div>
   )
