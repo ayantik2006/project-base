0aed90e5ed71d9d9426c41277dd03e9855e9a74b
Ayantik Sarkar <99005057+ayantik2006@users.noreply.github.com>
2025-09-27 14:34:17 +0530

    update index.js

--- backend/index.js
+++ backend/index.js
@@ -4,6 +4,7 @@ require("dotenv").config();
 const authRoutes=require("./routes/auth.js")
 const cookieParser=require("cookie-parser");
 const mongodb = require("./config/db.js");
+const path = require('path');
 
 const app = express();
 const PORT = process.env.PORT;
@@ -11,6 +12,7 @@ const SECRET_KEY=process.env.SECRET_KEY;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, '../frontend/build')));
 const frontendURL="https://project-base-frontend.onrender.com";
 app.use(
   cors({
@@ -23,6 +25,9 @@ mongodb();
 app.set("view engine","ejs");
 app.use(cookieParser(SECRET_KEY));
 app.use("/auth",authRoutes);
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../frontend/build', 'index.html'));
+});
 
 app.listen(PORT, () => {
   console.log("server live");
