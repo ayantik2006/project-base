b02217cfcc27a2970e8f9e951a4f4a361f656375
Ayantik Sarkar <99005057+ayantik2006@users.noreply.github.com>
2025-09-27 14:29:43 +0530

    update Feed component

--- frontend/src/components/Feed.tsx
+++ frontend/src/components/Feed.tsx
@@ -1,35 +1,5 @@
-import { useEffect, useState } from "react";
-import { useLocation, useNavigate } from "react-router-dom";
-import Signin from "./Signin";
-
 function Feed() {
-  const navigate = useNavigate();
-  const location = useLocation();
-  const backendURL = "https://project-base-backend.onrender.com";
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
-
-  useEffect(() => {
-    fetch(backendURL + "/auth/user", {
-      method: "POST",
-      credentials: "include",
-      headers: { "Content-Type": "application/json" },
-    })
-      .then((res) => res.json())
-      .then((res) => {
-        if (res.msg === "logged out") {
-          setIsLoggedIn(false);
-          navigate("/signin");
-        } else if (res.msg !== "logged out") {
-          setIsLoggedIn(true);
-        }
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-  }, [navigate, location.pathname]);
-
-  if (isLoggedIn) return <div>Feed</div>;
-  else return <Signin/>
+ return <div>Feed</div>;
 }
 
 export default Feed;
