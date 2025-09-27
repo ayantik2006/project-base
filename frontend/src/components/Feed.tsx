2ede08793c0af840256db4686a0f47cefecee3d3
Ayantik Sarkar <99005057+ayantik2006@users.noreply.github.com>
2025-09-27 14:22:43 +0530

    update Feed component

--- frontend/src/components/Feed.tsx
+++ frontend/src/components/Feed.tsx
@@ -5,31 +5,29 @@ function Feed() {
   const navigate = useNavigate();
   const location = useLocation();
   const backendURL = "https://project-base-backend.onrender.com";
-  const [isLoggedIn, setIsLoggedIn]=useState(false);
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
 
   useEffect(() => {
-      fetch(backendURL + "/auth/user", {
-        method: "POST",
-        credentials: "include",
-        headers: { "Content-Type": "application/json" },
+    fetch(backendURL + "/auth/user", {
+      method: "POST",
+      credentials: "include",
+      headers: { "Content-Type": "application/json" },
+    })
+      .then((res) => res.json())
+      .then((res) => {
+        if (res.msg === "logged out") {
+          setIsLoggedIn(false);
+          navigate("/signin");
+        } else if (res.msg !== "logged out") {
+          setIsLoggedIn(true);
+        }
       })
-        .then((res) => res.json())
-        .then((res) => {
-          if (res.msg === "logged out") {
-            setIsLoggedIn(false);
-          } else if (res.msg !== "logged out") {
-            setIsLoggedIn(true);
-          }
-        })
-        .catch((err) => {
-          console.log(err);
-        });
-    }, [navigate, location.pathname]);
-  
-  if(isLoggedIn)return (
-    <div>Feed</div>
-  )
-  else navigate("/signin");
+      .catch((err) => {
+        console.log(err);
+      });
+  }, [navigate, location.pathname]);
+
+  if (isLoggedIn) return <div>Feed</div>;
 }
 
-export default Feed
+export default Feed;
