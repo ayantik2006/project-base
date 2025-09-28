71b35383029f118c866dad61edb67abe631d891d
Ayantik Sarkar <99005057+ayantik2006@users.noreply.github.com>
2025-09-28 08:33:16 +0530

    google oauth step 1

--- backend/lib/oauth/google.js
+++ backend/lib/oauth/google.js
@@ -1,3 +1,7 @@
-import { Google } from "arctic";
+const { Google } = require("arctic");
 
-export const google=new Google(process.env.GOOGLE_CLIENT_ID,process.env.GOOGLE_CLIENT_SECRET,"https://project-base-backend.onrender.com/auth/google/callback")
+exports.google = new Google(
+  process.env.GOOGLE_CLIENT_ID,
+  process.env.GOOGLE_CLIENT_SECRET,
+  "https://project-base-backend.onrender.com/auth/google/callback"
+);
