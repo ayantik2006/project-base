2298f9c101cb1f77245579bcbb7a53b639beeec5
Ayantik Sarkar <99005057+ayantik2006@users.noreply.github.com>
2025-10-01 15:57:21 +0530

    profile section+edit profile feature done

--- frontend/src/components/Feed.tsx
+++ frontend/src/components/Feed.tsx
@@ -1,27 +1,55 @@
 import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
+import { Button } from "./ui/button";
 
 function Feed() {
   const backendURL = import.meta.env.VITE_BACKEND_URL;
   const navigate = useNavigate();
 
-  useEffect(()=>{
-    fetch(backendURL+"/me/name-username",{
-      method:"POST",
-      credentials:"include",
-      headers:{"Content-Type":"application/json"}
+  useEffect(() => {
+    fetch(backendURL + "/me/name-username", {
+      method: "POST",
+      credentials: "include",
+      headers: { "Content-Type": "application/json" },
     })
-    .then((res)=>res.json())
-    .then((res)=>{
-      if(res.msg==="no name username"){
-        navigate("/me/profile");
-      }
-    })
-    .catch((err)=>{console.log(err);})
-  },[]); 
-   
+      .then((res) => res.json())
+      .then((res) => {
+        if (res.msg === "no name username") {
+          navigate("/me/profile");
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+
   return (
     <div>
+      <Button
+        className="m-5 cursor-pointer"
+        variant={"outline"}
+        onClick={() => {
+          fetch(backendURL + "/auth/signout", {
+            method: "POST",
+            credentials: "include",
+            headers: { "Content-Type": "application/json" },
+          })
+          .then(()=>{navigate("/signin")})
+          .catch((err)=>{console.log(err)})
+        }}
+      >
+        Sign out
+      </Button>
+
+      <Button
+        className="m-5 cursor-pointer"
+        variant={"outline"}
+        onClick={() => {
+          navigate("/me/profile");
+        }}
+      >
+        My profile
+      </Button>
     </div>
   );
 }
